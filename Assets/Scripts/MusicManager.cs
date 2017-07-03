e5e41817c02c817dc41fa467f832f40b7946e14b
Thien Nguyen <thtnguyen93@gmail.com>
2017-07-02 21:28:22 -0700

    first commit

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour {
+
+    public AudioClip[] levelMusicChangeArray;
+    private AudioSource audioSource;
+	// Use this for initialization
+	void Start ()
+    {
+        audioSource = GetComponent<AudioSource>();
+	}
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        Debug.Log("Don't destroy on load: " + gameObject + "\n");
+        Debug.Log("Don't destroy on name: " + name + "\n");
+
+    }
+
+    public void ChangeVolume(float value)
+    {
+        audioSource.volume = value;
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+
+	}
+
+    private void OnLevelWasLoaded(int level)
+    {
+        AudioClip thisLevelMusic = levelMusicChangeArray[level];
+        Debug.Log("Playing clip: " + thisLevelMusic);
+        if (thisLevelMusic) // if there is some music attached
+        {
+            audioSource.clip = thisLevelMusic;
+            audioSource.loop = true;
+            audioSource.Play();
+        }
+    }
+}
---
+++ Assets/Scripts/OptionsController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsController : MonoBehaviour {
+
+    public Slider VolumeSlider;
+    public Slider DifficultySlider;
+    public LevelManager LevelManager;
+
+    private MusicManager musicManager;
+
+	// Use this for initialization
+	void Start ()
+    {
+        musicManager = GameObject.FindObjectOfType<MusicManager>();
+        VolumeSlider.value = PlayerPrefsManager.GetMasterVolume();
+        DifficultySlider.value = PlayerPrefsManager.GetDifficulty();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        musicManager.ChangeVolume(VolumeSlider.value);
+	}
+
+    public void SaveAndExit()
+    {
+        PlayerPrefsManager.SetMasterVolume(VolumeSlider.value);
+        PlayerPrefsManager.SetDifficulty(DifficultySlider.value);
+        LevelManager.LoadLevel("01a Start");
+    }
+
+    public void SetDefaults()
+    {
+        VolumeSlider.value = 0.8f;
+        DifficultySlider.value = 2f;
+    }
+}
